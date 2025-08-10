a39cc70c45f49cf5bcf1f8107f08c8f59b753522
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-08-10 21:25:10 +0100

    testimonial section

---
+++ components/TestimonialSection.tsx
@@ -0,0 +1,54 @@
+export default function TestimonialSection() {
+  return (
+    <section className="pb-20">
+      <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="text-center">
+          {/* User Avatars */}
+          <div className="flex justify-center items-center mb-8">
+            <div className="flex -space-x-2">
+              {/* Avatar 1 - Green background */}
+              <div className="w-12 h-12 sm:w-16 sm:h-16 rounded-full bg-green-400 border-4 border-white shadow-lg overflow-hidden">
+                <div className="w-full h-full bg-gradient-to-br from-green-300 to-green-500 flex items-center justify-center">
+                  <div className="w-8 h-8 sm:w-12 sm:h-12 bg-green-600 rounded-full"></div>
+                </div>
+              </div>
+              
+              {/* Avatar 2 - Purple background */}
+              <div className="w-12 h-12 sm:w-16 sm:h-16 rounded-full bg-purple-400 border-4 border-white shadow-lg overflow-hidden">
+                <div className="w-full h-full bg-gradient-to-br from-purple-300 to-purple-500 flex items-center justify-center">
+             
+  );
+}section>
+    </   </div>/div>
+   
+        <kquote>loc  </b      "
+  lready!!t to me aing ouchmpanies reather coise, and omazon, W from At recruiters"I go         
+   xed px-4">g-rela leadin00 italict-gray-7dium tex3xl font-med:text-:text-2xl m"text-xl smme=classNaote blockqu
+          < Quote */}Testimonial {/*   
+       v>
+/di       <  
+    </div>    >
+      </div             </div>
+            >
+    divull"></d-f00 rounde-6g-yellow2 bh-1:w-12 sm:sm8 h-me="w-8 Naclassv   <di               >
+ -center"stify ju-centermsflex ite-500 lowyelto-ellow-300 br from-y-to--gradientl h-full bge="w-fullassNamdiv c       <  ">
+       denflow-hiddow-lg overite shar-wh4 borde-400 border-llowyefull bg-nded-:h-16 rouw-16 sm12 sm:e="w-12 h-lassNam      <div c}
+        ground */backYellow Avatar 5 - *      {/                
+        </div>
+        v>
+      </di              
+ iv>ll"></d rounded-fublue-6002 bg-m:h-1-8 sm:w-12 s"w-8 hName=v class    <di              r">
+y-centeustifcenter jlex items- fe-500-blublue-300 to-br from-adient-toull bg-gr"w-full h-flassName=     <div c           den">
+-hidg overflowdow-lte shar-whir-4 bordee-400 borde bg-blu-fullunded6 sm:h-16 row-1-12 h-12 sm:ame="w classNdiv     <        ound */}
+  Blue backgrvatar 4 -{/* A                 
+ 
+          /div>     <         >
+       </div        </div>
+ ed-full">roundorange-600 h-12 bg-:w-12 sm:8 smme="w-8 h-assNaiv cl    <d              
+center">tify-s-center jus00 flex itemange-5-or300 torom-orange-o-br f-tent-gradill bgull h-fue="w-fv classNam     <di       
+    ">ow-hiddenoverflshadow-lg white -4 border-rdere-400 bo-orang bged-fullound16 rm:w-16 sm:h-2 s12 h-1"w-className= <div            */}
+   ackgroundrange bown/Otar 3 - Br* Ava{/          
+                     </div>
+           /div>
+         <    /div>
+   ><l"-fulroundedpurple-600 g--12 b-12 sm:h-8 sm:ww-8 hsName="div clas     <
